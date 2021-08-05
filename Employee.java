a739fa505ba076da8bb239fa511afd73402c885d
Animesh71 <rajputanimesh71@gmail.com>
2021-08-05 15:28:56 +0530

    Class Second Commit

--- Employee.java
+++ Employee.java
@@ -1,3 +1,18 @@
-public class Employee
-{
+public class User{
+  public static void main(String[]args){
+    System.out.println("Hello From User");
+    System.out.println("Is number Prime ? :"+ isPrime(11));
 }
+  Public static int isPrime(int num)
+{
+for(int i=2;i<num/2;i++)
+{
+if(num%i==0)
+{
+return false;
+break
+}
+return true;
+}
+}
+     
